6931b3987a460d6f5ce063159d30b2d7ffdd2236
UnreachableWebServer <149638054+UnreachableWebServer@users.noreply.github.com>
2023-11-10 18:56:14 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const themeSwitchers = document.querySelectorAll('.change-theme');
+
+themeSwitchers.forEach(switcher => {
+    switcher.addEventListener('click', function() {
+        applyTheme(this.dataset.theme)
+        localStorage.setItem('theme', this.dataset.theme)
+    })
+})
+
+function applyTheme(themeName) {
+    let themeUrl = `css/theme-${themeName}.css`
+    document.querySelector('[title="theme"]').setAttribute('href', themeUrl)
+}
+
+let activeTheme = localStorage.getItem('theme')
+
+if (activeTheme == null) {
+    applyTheme('light')
+} else {
+    applyTheme(activeTheme)
+}
+
+function errorMusic(){
+    var musicError = new Audio('sounds/error_sound.mp3');
+    musicError.play();
+}
+
+function clickMusic() {
+    var musicClick = new Audio('sounds/button_click.mp3');
+    musicClick.play();
+}
